c1f80ec7d862e6407ba872cc601c1098c58ea144
saitoxu <yosuke.saito1202@gmail.com>
2016-10-12 20:20:13 +0900

    POST: test

--- app.rb
+++ app.rb
@@ -2,15 +2,40 @@ require 'sinatra'
 require 'twitter'
 require 'json'
 
+get '/' do
+end
+
 post '/payload' do
+
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
     config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
     config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
     config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
   end
+  count = 0
+  prefix = 'POST:'
+  commits = JSON.parse(request.body.read)['commits']
 
-  # client.update("test")
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+  for commit in commits do
+    message = commit['message']
+    if message.start_with?(PREFIX)
+      title = message.sub(/${prefix}/, '').strip
+      addedFiles = commit['message']
+      for added in addedFiles do
+        if md = added.match(/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*).md/)
+          year  = md[1]
+          month = md[2]
+          day   = md[3]
+          page  = md[4] + '.html'
+          url = ['http://saitoxu.io/blog/', year, month, day, page].join('/')
+
+          client.update(title + "\n" + url)
+          count += 1
+        end
+      end
+    end
+  end
+  puts "#{count} tweet(s) posted."
+
 end
